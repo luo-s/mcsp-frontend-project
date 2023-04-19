c13aa61a980d57a91a316c5b73857876eb05ff2f
Shu <luo.s@outlook.com>
2023-04-19 13:34:50 -0800

    working on DOM

--- app.js
+++ app.js
@@ -24,31 +24,25 @@ function getIt(defaultInput) {
     }
   );
 }
-
+// event listener
 $("#yosemite").on("click", function () {
   getIt("yose");
 });
-
 $("#yellowStone").on("click", function () {
   getIt("yell");
 });
-
 $("#arch").on("click", function () {
   getIt("arch");
 });
-
 $("#grandTeton").on("click", function () {
   getIt("grte");
 });
-
 $("#joshuaTree").on("click", function () {
   getIt("jotr");
 });
-
 $("#zion").on("click", function () {
   getIt("zion");
 });
-
 $("#search").on("click", function () {
   getIt();
 });
@@ -61,21 +55,43 @@ function update(data) {
     const image = ele.images[0].url;
     const directions = ele.directionsUrl;
     const directionsInfo = ele.directionsInfo;
+    const address = ele.addresses[0];
+    let phoneNumber = ele.contacts.phoneNumbers[0].phoneNumber;
+    phoneNumber = phoneNumber.split("/").join("-");
+    const emailAddress = ele.contacts.emailAddresses[0].emailAddress;
 
-    //                    <img class="npImg" src=${image}>
+    // <img class="npImg" src=${image}>
 
-    const html = `<span class="nationalPark">
-                    <h3 class="npName">
-                      <a href=${url}>${name}</a>
-                    </h3>
-                    <h3 class="npDescription">${description}</h3>
-                    <div class="directions">
-                      <a href=${directions}>Get Directions</a>
-                      <p>
-                        ${directionsInfo}
-                      </p>
-                    </div>
-                  </span>`;
-    $("#carouselExampleDark").append($(`${html}`));
+    const html = `
+    <div class="card" style="width: 80%;">
+    <img src="${image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <a class="card-title" href="${url}" style="font-size:40px"">${name}</a>
+      <p class="card-text">${description}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${address.line1} ${address.city} ${address.stateCode} ${address.postalCode}</li>
+      <li class="list-group-item">Contact number: ${phoneNumber}</li>
+      <li class="list-group-item">email: ${emailAddress}</li>
+    </ul>
+    <div class="card-body">
+      <a href="https://en.wikipedia.org/wiki/${name}" class="card-link">Wikipedia</a>
+      <a href="${directions}" class="card-link">Directions</a>
+    </div>
+  </div>
+    `;
+    // const html = `<span class="nationalPark">
+    //                 <h3 class="npName">
+    //                   <a href=${url}>${name}</a>
+    //                 </h3>
+    //                 <h3 class="npDescription">${description}</h3>
+    //                 <div class="directions">
+    //                   <a href=${directions}>Get Directions</a>
+    //                   <p>
+    //                     ${directionsInfo}
+    //                   </p>
+    //                 </div>
+    //               </span>`;
+    $("body").append($(`${html}`));
   }
 }
