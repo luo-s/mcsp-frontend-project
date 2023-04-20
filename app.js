b27cffa8617978782b1398306bcba016861077a4
Shu <luo.s@outlook.com>
2023-04-19 16:07:53 -0800

    add video

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function getIt(defaultInput) {
   $("#cards").empty();
   $("#carouselExampleDark").empty();
+  $("#results").empty();
   let input = $("#input").val();
   if (defaultInput !== undefined) {
     input = defaultInput;
@@ -54,44 +55,80 @@ function update(data) {
     const description = ele.description;
     const image = ele.images[0].url;
     const directions = ele.directionsUrl;
-    const directionsInfo = ele.directionsInfo;
     const address = ele.addresses[0];
-    let phoneNumber = ele.contacts.phoneNumbers[0].phoneNumber;
-    phoneNumber = phoneNumber.split("/").join("-");
+    const num = ele.contacts.phoneNumbers[0].phoneNumber.replace(/\D/g, "");
+    const phoneNumber =
+      num.slice(0, 3) + "-" + num.slice(3, 6) + "-" + num.slice(6, 10);
     const emailAddress = ele.contacts.emailAddresses[0].emailAddress;
-
-    // <img class="npImg" src=${image}>
+    const parkCode = ele.parkCode;
 
     const html = `
-    <div class="card" style="width: 80%;">
-    <img src="${image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <a class="card-title" href="${url}" style="font-size:40px"">${name}</a>
-      <p class="card-text">${description}</p>
+    <div class="card" id="${parkCode}" style="width: 80%;">
+      <img src="${image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <a class="card-title" href="${url}" style="font-size:40px" target="_blank"">${name}</a>
+        <p class="card-text">${description}</p>
+       </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">${address.line1}, ${address.city}, ${address.stateCode} ${address.postalCode}</li>
+        <li class="list-group-item">Contact Number: ${phoneNumber} | Email: ${emailAddress}</li>
+      </ul>
+      <div class="card-body">
+        <a href="https://en.wikipedia.org/wiki/${name}" class="card-link" target="_blank">Wikipedia</a>
+        <a href="${directions}" class="card-link" target="_blank">Directions</a>
+      </div>
     </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">${address.line1} ${address.city} ${address.stateCode} ${address.postalCode}</li>
-      <li class="list-group-item">Contact number: ${phoneNumber}</li>
-      <li class="list-group-item">email: ${emailAddress}</li>
-    </ul>
-    <div class="card-body">
-      <a href="https://en.wikipedia.org/wiki/${name}" class="card-link">Wikipedia</a>
-      <a href="${directions}" class="card-link">Directions</a>
-    </div>
-  </div>
     `;
-    // const html = `<span class="nationalPark">
-    //                 <h3 class="npName">
-    //                   <a href=${url}>${name}</a>
-    //                 </h3>
-    //                 <h3 class="npDescription">${description}</h3>
-    //                 <div class="directions">
-    //                   <a href=${directions}>Get Directions</a>
-    //                   <p>
-    //                     ${directionsInfo}
-    //                   </p>
-    //                 </div>
-    //               </span>`;
-    $("body").append($(`${html}`));
+    $("#results").append($(`${html}`));
+
+    // add videos
+    if (parkCode === "yose") {
+      let iframeYose = document.createElement("iframe");
+      iframeYose.id = "yoseVideo";
+      iframeYose.src = "https://www.youtube.com/embed/s9M30w085SY";
+      iframeYose.width = "100%";
+      iframeYose.height = "600";
+      $("#yose").append($(iframeYose));
+    }
+    if (parkCode === "yell") {
+      let iframeYell = document.createElement("iframe");
+      iframeYell.id = "yellVideo";
+      iframeYell.src = "https://www.youtube.com/embed/3RDYVVmVR2U";
+      iframeYell.width = "100%";
+      iframeYell.height = "600";
+      $("#yell").append($(iframeYell));
+    }
+    if (parkCode === "arch") {
+      let iframeArch = document.createElement("iframe");
+      iframeArch.id = "archVideo";
+      iframeArch.src = "https://www.youtube.com/embed/B4UKkCwbAmw";
+      iframeArch.width = "100%";
+      iframeArch.height = "600";
+      $("#arch").append($(iframeArch));
+    }
+    if (parkCode === "zion") {
+      let iframeZion = document.createElement("iframe");
+      iframeZion.id = "zionVideo";
+      iframeZion.src = "https://www.youtube.com/embed/ecc7t3PkkiM";
+      iframeZion.width = "100%";
+      iframeZion.height = "600";
+      $("#zion").append($(iframeZion));
+    }
+    if (parkCode === "jotr") {
+      let iframeJotr = document.createElement("iframe");
+      iframeJotr.id = "jotrVideo";
+      iframeJotr.src = "https://www.youtube.com/embed/MQrSgsy6yHQ";
+      iframeJotr.width = "100%";
+      iframeJotr.height = "600";
+      $("#jotr").append($(iframeJotr));
+    }
+    if (parkCode === "grte") {
+      let iframeGrte = document.createElement("iframe");
+      iframeGrte.id = "grteVideo";
+      iframeGrte.src = "https://www.youtube.com/embed/oRxLvMHLI3I";
+      iframeGrte.width = "100%";
+      iframeGrte.height = "600";
+      $("#grte").append($(iframeGrte));
+    }
   }
 }
